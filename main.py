92243959f1ed6fd648de1948cf6878027622eb50
Agustín Vargas Toro <fustincho@tuta.io>
2024-11-28 09:04:36 -0500

    update(game): added player sprite, hitbox, and rotation

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-import pygame
 import constants
+import pygame
+from player import Player
+
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              return
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+
+        # 60 FPS. Tick returns ms so we divide by 1000
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
